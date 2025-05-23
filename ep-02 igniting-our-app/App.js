21e5095afb4fc0a52a2a200f28e37d47e273b63b
umeshpalsingh <umeshpal6789@gmail.com>
2025-05-23 17:20:09 +0530

    code update

---
+++ ep-02 igniting-our-app/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const headingContent = "Hello World from React";
+
+const heading = React.createElement("h1", {id: "heading", className: "heading"}, headingContent);
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, 
+        [React.createElement("h1", {}, "Hello I am H1"), React.createElement("h2", {}, "Hello I am H2 Haha")]
+    ),
+    React.createElement("div", {id: "child2"}, 
+        [React.createElement("h3", {}, "Hello I am H3"), React.createElement("h4", {}, "Hello I am H4")]
+    )
+]);
+
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(parent);
